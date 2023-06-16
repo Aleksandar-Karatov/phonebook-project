509b76f5fa4bf4de4af958a909ed4d5b7ca9aed7
ytopchieva <iana0107@abv.bg>
2023-06-16 15:33:40 +0300

    front-end version1

---
+++ front-end/index.js
@@ -0,0 +1,84 @@
+document.addEventListener('DOMContentLoaded', function (){
+    const button = document.querySelector('.addContact');
+    button.addEventListener('click', goToForm);
+
+    function goToForm() {
+        window.location.href = "addconctact.html";
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.querySelector('.button input');
+  
+    const formInputs = document.querySelectorAll('.user .input input');
+    formInputs.forEach(function (input) {
+      input.addEventListener('input', function () {
+        validateForm();
+      });
+});
+  
+    function validateForm() {
+      const firstname = document.getElementById('firstname').value;
+      const lastname = document.getElementById('lastname').value;
+      const address = document.getElementById('address').value;
+      const email = document.getElementById('email').value;
+      const phone = document.getElementById('phone').value;
+  
+      let isValid = true;
+  
+      //firstname
+      if (firstname.trim() === '') {
+        document.getElementById('firstname').classList.add('error');
+        isValid = false;
+      } else {
+        document.getElementById('firstname').classList.remove('error');
+      }
+  
+      //lastname
+      if (lastname.trim() === '') {
+        document.getElementById('lastname').classList.add('error');
+        isValid = false;
+      } else {
+        document.getElementById('lastname').classList.remove('error');
+      }
+  
+      //address
+      if (address.trim() === '') {
+        document.getElementById('address').classList.add('error');
+        isValid = false;
+      } else {
+        document.getElementById('address').classList.remove('error');
+      }
+  
+      //email
+      const emailRegex = /^\S+@\S+\.\S+$/;
+      if (!emailRegex.test(email)) {
+        document.getElementById('email').classList.add('error');
+        isValid = false;
+      } else {
+        document.getElementById('email').classList.remove('error');
+      }
+  
+      // phone number
+      const phoneRegex = /^\d{10}$/;
+      if (!phoneRegex.test(phone)) {
+        document.getElementById('phone').classList.add('error');
+        isValid = false;
+      } else {
+        document.getElementById('phone').classList.remove('error');
+      }
+  
+      //btn
+      if (firstname.trim() === '' || lastname.trim() === '' || address.trim() === '' || email.trim() === '' || phone.trim() === '') {
+        addButton.disabled = true;
+      } else {
+        addButton.disabled = false;
+      }
+  
+      return isValid;
+    }
+  });
+
+    
+   
+
